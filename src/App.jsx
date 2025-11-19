c576c063f6292802f8944d1e2f8381cdb71d2add
guitch95 <guitch95@gmail.com>
2025-11-19 13:55:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Offer from './pages/Offer';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offer />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Offer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import {useParams} from 'react-router-dom';
+
+const Offer = () => {
+  // const {id} = useParams();
+
+  return <div>Offer</div>;
+};
+
+export default Offer;
