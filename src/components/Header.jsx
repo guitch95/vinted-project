7977aa982a97080d5ed5d2b79a4e7af04d42eeba
guitch95 <guitch95@gmail.com>
2025-11-20 10:17:24 +0100

    Worked on the CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../assets/img/logo.png';
 import {Link} from 'react-router-dom';
+import {FaSearch} from 'react-icons/fa';
 
 const Header = () => {
   return (
@@ -10,6 +11,14 @@ const Header = () => {
           <Link to="/">
             <img src={logo} alt="logo vinted" />
           </Link>
+        </div>
+        <div className="container-search">
+          <FaSearch color="#7a7a7aff" />
+          <input type="text" placeholder="Recherche des articles" />
+        </div>
+        <div className="button-container">
+          <button>S'inscrire</button>
+          <button>Se connecter</button>
         </div>
       </div>
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         const response = await axios.get(
           'https://lereacteur-vinted-api.herokuapp.com/offers'
         );
-        console.log(response.data);
+        // console.log(response.data);
         setData(response.data);
         setIsLoading(false);
       } catch (error) {
